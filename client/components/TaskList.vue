0834028bbb474c27fb6d10b8d77eaedfc9eace6f
QiongrongJiang <qiongrongjiang@gmail.com>
2018-02-17 00:44:38 +0800

    tweaks

--- client/components/TaskList.vue
+++ client/components/TaskList.vue
@@ -1,43 +1,46 @@
 <template>
-  <div>
-    <div class="task-list__header-container cursor--pointer">
-      <div
-        class="task-list__header"
-        @click="toggleListExpansion(list)">
-        <i
-          class="el-icon-caret-bottom task-list__header-caret"
-          :style="{ transform: caretRotate }">
-        </i>
-        <span>{{ list.title }}</span>
+  <collapse-transition>
+    <div v-if="activeNavId === '4' ? list.content.length : list.content.filter(item => { return item.is_done === false }).length">
+      <div class="task-list__header-container cursor--pointer">
+        <div
+          class="task-list__header"
+          @click="toggleListExpansion(list)">
+          <i
+            class="el-icon-caret-bottom task-list__header-caret"
+            :style="{ transform: caretRotate }">
+          </i>
+          <span>{{ list.title }}</span>
+        </div>
       </div>
-    </div>
 
-    <collapse-transition>
-      <div
-        class="task-list__content"
-        v-if="showList">
-        <!-- <draggable v-model="list.content" :options="{ group:'task' }" @end="onEndDrag">
+      <collapse-transition>
+        <div
+          class="task-list__content"
+          v-if="showList">
+          <!-- <draggable v-model="list.content" :options="{ group:'task' }" @end="onEndDrag">
+            <task-item
+              v-for="(item, index) in list.content"
+              :key="index"
+              :task="item">
+            </task-item>
+          </draggable> -->
+
           <task-item
-            v-for="(item, index) in list.content"
-            :key="index"
+            v-for="item in list.content"
+            :key="item.id"
             :task="item">
           </task-item>
-        </draggable> -->
-
-        <task-item
-          v-for="(item, index) in list.content"
-          :key="index"
-          :task="item">
-        </task-item>
-      </div>
-    </collapse-transition>
-  </div>
+        </div>
+      </collapse-transition>
+    </div>
+  </collapse-transition>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
 import TaskItem from 'components/TaskItem'
 import CollapseTransition from 'components/CollapseTransition'
+import { mapState } from 'vuex'
 
 export default {
   components: {
@@ -70,7 +73,10 @@ export default {
       console.log(e)
       console.log(this.list)
     }
-  }
+  },
+  computed: mapState({
+    activeNavId: state => state.navId
+  })
 }
 </script>
 
