0cd1521ee32075dae4d1c0b019ed0f226b725459
QiongrongJiang <qiongrongjiang@gmail.com>
2018-02-16 23:48:00 +0800

    add empty input icon

--- client/components/Search.vue
+++ client/components/Search.vue
@@ -1,31 +1,46 @@
 <template>
-  <div v-click-outside="hideSearch">
+  <flex-box v-click-outside="hideSearch">
     <i
       class="el-icon-search search__icon icon"
       @click="showSearch">
     </i>
 
-    <el-input
-      ref="taskSearch"
-      size="small"
-      placeholder="Search task"
-      v-model="searchInput"
-      class="search__input"
-      :style="searchStyle"
-      @input.native="search">
-    </el-input>
-  </div>
+    <div
+      class="search__input-container"
+      :style="searchStyle">
+      <el-input
+        ref="taskSearch"
+        size="small"
+        placeholder="Search task"
+        v-model="searchInput"
+        class="search__input"
+        @input.native="search">
+      </el-input>
+
+      <i
+        class="el-icon-close search__icon search__icon-close"
+        v-if="showDeleteIcon"
+        @click="clearSearchInput">
+      </i>
+    </div>
+  </flex-box>
 </template>
 
 <script>
+import FlexBox from 'components/Layout/FlexBox'
+
 export default {
+  components: {
+    FlexBox
+  },
   data () {
     return {
       searchInput: '',
       searchStyle: {
         width: '0px',
         marginLeft: '0px'
-      }
+      },
+      showDeleteIcon: false
     }
   },
   methods: {
@@ -43,7 +58,16 @@ export default {
       this.$refs.taskSearch.focus()
     },
     search () {
+      if (this.searchInput.length) {
+        this.showDeleteIcon = true
+      } else {
+        this.showDeleteIcon = false
+      }
       this.$emit('search', this.searchInput)
+    },
+    clearSearchInput () {
+      this.searchInput = ''
+      this.$emit('search', '')
     }
   }
 }
@@ -54,7 +78,9 @@ export default {
   font-size: 16px;
 }
 
-.search__input.el-input {
+.search__input-container {
+  overflow: hidden;
+  position: relative;
   transition: all .3s;
 }
 
@@ -62,6 +88,14 @@ export default {
   border: 0;
   border-bottom: 1px solid #d9d9d9;
   border-radius: 0;
-  padding: 0;
+  padding: 0 20px 0 0;
+}
+
+.search__icon-close {
+  color: #aaa;
+  cursor: pointer;
+  position: absolute;
+  top: 8px;
+  right: 2px;
 }
 </style>
